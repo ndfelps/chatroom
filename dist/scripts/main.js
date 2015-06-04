206da5b1bd9a3caee681464acefc1fde875aad95
Nathaniel Felps <ndfelps@gmail.com>
2015-06-04 17:02:54 -0500

    dist/

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,34 +3,88 @@ $(document).on('ready', start)
 function start() {
 	var routerConfig = {
 		routes: {
-			"": 'welcome',
+			"chat1": "nav1",
+			"chat2": "nav2",
+			"chat3": "nav3",
+			"chat4": "nav4"
+		},
+		nav1: function() {
+			$('.page').hide();
+			$('#chatArea1').show();
+		},
+		nav2: function() {
+			$('.page').hide();
+			$('#chatArea2').show();
+		},
+		nav3: function() {
+			$('.page').hide();
+			$('#chatArea3').show();
+		},
+		nav4: function() {
+			$('.page').hide();
+			$('#chatArea4').show();
 		}
 	}
 
-	$('.btn').on('click', messSubmit)
+	var app = Backbone.Router.extend(routerConfig);
+	var myRouter = new app();
+	Backbone.history.start();
+
+	$('.btn').on('click', messSub);
+	$('#username').on('keyup', messSubPush);
+	$('#message').on('keyup', messSubPush);
 	var $username = $("#username");
 	var $message = $("#message");
+	getMess();
 
-	function messSubmit(e) {
+	function messSub(e) {
+		if($("#username").val() === '' || $("#message").val() === '') {
 
-		var myMessage = {
-			username: $username.val(),
-			text: $message.val()
+		} else {
+			var myMessage = {
+				name: $("#username").val(),
+				message: $("#message").val(),
+				ch: window.location.hash
+			}
+
+			$.post(
+				'http://tiny-pizza-server.herokuapp.com/collections/theWaitresses/',
+				myMessage
+			);
 		}
+	}
+	function messSubPush(e) {
+		if($("#username").val() === '' || $("#message").val() === '') {
 
-		$.post(
-			'http://tiny-pizza-server.herokuapp.com/collections/theWaitresses/',
-			myMessage
-		);
-		console.log("ugh");
-		console.log(myMessage.message)
+		} else if(event.keyCode === 13) {
+				var myMessage = {
+					name: $("#username").val(),
+					message: $("#message").val(),
+					ch: window.location.hash
+				}
+
+				$.post(
+					'http://tiny-pizza-server.herokuapp.com/collections/theWaitresses/',
+					myMessage
+				);
+				$('#message').val('');
+			}
 	}
 
-	function getMessages() {
+	function getMess() {
 		$.get(
 			'http://tiny-pizza-server.herokuapp.com/collections/theWaitresses/',
 			onMessagesReceived,
 			'json'
 		);
+	}
+	setInterval(getMess, 500);
+
+	function onMessagesReceived(val) {
+		$(window.location.hash).html('');
+			for (var i = val.length; i>0; --i) {
+				if(window.location.hash === val[i-1].ch)
+					$(window.location.hash).append('<div>' + val[i-1].name + ': ' + val[i-1].message + '</div>')
+			}
 	}
 }
